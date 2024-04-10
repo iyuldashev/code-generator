7a697051847f58d5975b626485e19a5a40e24d86
Izzatillo Yuldashev <izzatilloyuldashev@MacBook-Pro-de-Izzatillo.local>
2024-04-10 23:17:06 +0900

    first commit

---
+++ code-generator.py
@@ -0,0 +1,5 @@
+import random
+passlength = int(input("Enter the length of password: "))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlength ))
+print(p)
